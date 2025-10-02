f7ec16d1a961433b219d7e5d3df99d2a14e07c8e
huiyi0122 <huiyi.chai@devsquad.my>
2025-10-02 16:40:25 +0800

    server ip address

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,5 +14,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running at http://0.0.0.0:3000");
+  console.log("Server running at http://192.168.0.18:3000");
 });
